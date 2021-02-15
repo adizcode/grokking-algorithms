2f1dfeb69da8eed42c33c7071835bfc3025d41e3
Advitiay Anand <42904676+adizcode@users.noreply.github.com>
2021-02-16 00:23:27 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+class Implementation {
+
+    int factorial(int x) {
+
+        // Base case
+        if (x == 1) {
+            return 1;
+        }
+
+        // Recursive case
+        return x * factorial(x - 1);
+    }
+}
